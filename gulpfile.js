ea59de606ced9bb809f1074e603e9b45a31044b1
Francois Achache <francois.achache@gmail.com>
2018-02-19 18:13:46 +0800

    fix file to dist

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var gulp = require('gulp'),
 gulp.task('default', ['clean', 'build']);
 
 gulp.task('build', function () {
-	gulp.src('src/infinityscroll.css').pipe(gulp.dest('dist'));
-	return gulp.src('src/infinityscroll.js').pipe(gulp.dest('dist'));
+	gulp.src('src/boxesscroll.css').pipe(gulp.dest('dist'));
+	return gulp.src('src/boxesscroll.js').pipe(gulp.dest('dist'));
 });
 gulp.task('clean', function () {
 	return del.sync(['dist/**/*']);
